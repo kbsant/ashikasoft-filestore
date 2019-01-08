6343fced8e1918bcfdb49678ad0ef84699e4692c
kbsant <keansant@gmail.com>
2019-01-08 12:18:19 +0900

    project url

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ashikasoft/filestore "0.1.0-SNAPSHOT"
   :description "A file stor for prototypes or small projects"
-  :url "https://www.ashikasoft.com/"
+  :url "https://github.com/kbsant/ashikasoft-filestore"
   :dependencies [[org.clojure/clojure "1.10.0"]])
