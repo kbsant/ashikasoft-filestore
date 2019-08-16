cf080c87fbe868da3344e45d2790441efed22355
kbsant <keansant@gmail.com>
2019-08-16 11:55:14 +0900

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ashikasoft/filestore "0.1.0-SNAPSHOT"
+(defproject ashikasoft/filestore "0.1.0"
   :description "A file stor for prototypes or small projects"
   :url "https://github.com/kbsant/ashikasoft-filestore"
   :dependencies [[org.clojure/clojure "1.10.0"]])
