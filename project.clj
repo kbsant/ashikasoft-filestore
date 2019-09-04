4eb75b83614861cfd70cd30793beb52ed6264254
kbsant <keansant@gmail.com>
2019-09-05 00:58:11 +0900

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ashikasoft/filestore "0.1.1-SNAPSHOT"
   :description "A file store for prototypes or small projects"
   :url "https://github.com/kbsant/ashikasoft-filestore"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
